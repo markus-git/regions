36c7fea8c8ed3b4b7fab413d134e48adf01ec3af
Markus <mararon@chalmers.se>
2021-04-17 14:51:49 +0200

    Forgot to import 'interpretation'.

--- src/Language/Diorite.hs
+++ src/Language/Diorite.hs
@@ -2,7 +2,7 @@ module Language.Diorite
     ( module Language.Diorite.Signatures
     , module Language.Diorite.Syntax
     , module Language.Diorite.Sugar
-    --, module Language.Diorite.Interpretation
+    , module Language.Diorite.Interpretation
     , module Language.Diorite.Decoration
     --, module Language.Diorite.Traversal
     ) where
@@ -10,6 +10,6 @@ module Language.Diorite
 import Language.Diorite.Signatures
 import Language.Diorite.Syntax
 import Language.Diorite.Sugar
---import Language.Diorite.Interpretation
+import Language.Diorite.Interpretation
 import Language.Diorite.Decoration
 --import Language.Diorite.Traversal
