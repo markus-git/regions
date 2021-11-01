a9efd3263f575a7e90cb368e47f201771008ece2
Markus <mararon@chalmers.se>
2021-11-01 10:33:18 +0100

    WIP on example lang. for region annotation to see what works and what doesn't.

--- tests/Rgn.hs
+++ tests/Rgn.hs
@@ -13,61 +13,141 @@ import Language.Diorite.Interpretation
 import qualified Language.Diorite.Region.Annotation as A
 import qualified Language.Diorite.Region.Labels as L
 
+import Data.Typeable
+import Data.Constraint (Constraint)
+
+import GHC.TypeNats
+
 --------------------------------------------------------------------------------
--- * ...
+-- * Region annotation.
 --------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------
+-- ** Example language based on 'D'.
+
+type D :: Signature (L.Put Nat) * -> *
 data D a where
-    X :: Int -> D ('Const Int)
-    Y :: D ('Const Int ':-> 'Const Int)
-    Z :: D ('Const Int ':-> 'Const Int ':-> 'Const Int)
-    --
-    A :: D ('Const Int ':-> ('Const Int ':-> 'Const Int) ':-> 'Const Int)
+    -- Numerical
+    Num :: Int -> D ('Const Int)
+    Neg :: D ('Const Int ':-> 'Const Int)
+    Add :: D ('Const Int ':-> 'Const Int ':-> 'Const Int)
+    -- Sharing
+    Let :: D ('Const Int ':-> ('Const Int ':-> 'Const Int) ':-> 'Const Int)
+    -- Tuples
+    Tup :: (Typeable a, Typeable b) => D ('Const a ':-> 'Const b ':-> 'Const (a, b))
+    Fst :: (Typeable a, Typeable b) => D ('Const (a, b) ':-> 'Const a)
+    Snd :: (Typeable a, Typeable b) => D ('Const (a, b) ':-> 'Const b)
 
 instance Sym D where
-    symbol (X _) = signature
-    symbol (Y)   = signature
-    symbol (Z)   = signature
-    symbol (A)   = signature
+    symbol (Num _) = signature
+    symbol (Neg)   = signature
+    symbol (Add)   = signature
+    symbol (Let)   = signature
+    symbol (Tup)   = signature
+    symbol (Fst)   = signature
+    symbol (Snd)   = signature
 
 instance Render D where
-    renderSym (X i) = show i
-    renderSym (Y)   = "-"
-    renderSym (Z)   = "+"
-    renderSym (A)   = "let"
+    renderSym (Num i) = show i
+    renderSym (Neg)   = "-"
+    renderSym (Add)   = "+"
+    renderSym (Let)   = "let"
+    renderSym (Tup)   = "(,)"
+    renderSym (Fst)   = "1_"
+    renderSym (Snd)   = "2_"
+
+type TR :: * -> *
+data TR a where
+    TInt :: TR Int
+    TTup :: TR a -> TR b -> TR (a, b)
+
+type T :: * -> Constraint
+class (Eq a, Show a, Typeable a) => T a where
+    tyrep :: TR a
+
+instance T Int where
+    tyrep = TInt
+
+instance (T a, T b) => T (a, b) where
+    tyrep = TTup tyrep tyrep
 
 type B :: * -> *
-type B a = Beta D ('None :: Qualifier (L.Put *)) ('Const a)
+type B a = Beta (D :&: TR) ('None :: Qualifier (L.Put Nat)) ('Const a)
+
+smartD ::
+    forall
+       (es  :: Exists    (L.Put Nat))
+       (sub :: Signature (L.Put Nat) * -> *)
+       (sig :: Signature (L.Put Nat) *)
+       (f   :: *)
+    .  ( Sig sig
+       , Ex es
+       , f          ~ SmartBeta (D :&: TR) ('None) es sig
+       , es         ~ SmartEx  f
+       , sig        ~ SmartSig f
+       , (D :&: TR) ~ SmartSym f
+       , sub :<: D
+       , T (Result sig)
+       )
+    => sub sig -> f
+smartD = smartSymDecor tyrep
 
 --------------------------------------------------------------------------------
+-- ** Some functions and expressions in 'D'.
 
-int :: Int -> B Int
-int = smartSym' . X
+num :: Int -> B Int
+num = smartD . Num
 
 neg :: B Int -> B Int
-neg = smartSym' Y
+neg = smartD Neg
 
 add :: B Int -> B Int -> B Int
-add = smartSym' Z
+add = smartD Add
 
 share :: B Int -> (B Int -> B Int) -> B Int
-share = smartSym' A
+share = smartD Let
+
+tup :: (T a, T b) => B a -> B b -> B (a, b)
+tup = smartD Tup
+
+l_ :: (T a, Typeable b) => B (a, b) -> B a
+l_ = smartD Fst
+
+r_ :: (Typeable a, T b) => B (a, b) -> B b
+r_ = smartD Snd
 
 ex1 :: B Int
-ex1 = add (int 1) (neg (int 2))
+ex1 = add (num 1) (neg (num 2))
 
 ex2 :: B Int
-ex2 = share (int 2) (\x -> add (int 1) x)
+ex2 = share (num 2) (\x -> add (num 1) x)
+
+ex3 :: B Int
+ex3 = share (num 2) (\x -> l_ (tup x (add x (neg x))))
 
 --------------------------------------------------------------------------------
+-- ** Annotation of expressions in 'D'.
 
-type E a = A.EBeta (D :&: A.Ann) ((A.>=) 'None) ('Const @(L.Put *) a)
+-- type L :: forall p . Signature p * -> *
+-- data L sig where
+--     LTR :: TR a -> L ('Const a)
 
-ann :: B Int -> E Int
-ann = A.annotate
+-- int : () -> Int^a
+-- neg : Int^a -> Int^b
+-- add : Int^a, Int^b -> Int^c
+-- let : Int^a, (Int^a -> Int^b) -> Int^b
+-- ...
+-- instance A.Label D where
+--     type Lbl D = L
+--     label (X _) (A.Nil) = undefined
+--     label (Y) (_ A.:* A.Nil) = undefined
+--     label (Z) (_ A.:* _ A.:* A.Nil) = undefined
+--     label (A) (_ A.:* _ A.:* A.Nil) = undefined
 
-pr :: E Int -> IO ()
-pr (A.EBeta b _) = putStrLn (show b)
+--type EB a = A.Beta2 (A.L (D :+: L.Rgn)) ((A.>=) 'None) ('Const @(L.Put Nat) a)
+
+--ann :: B Int -> EB Int
+--ann = fst (A.annotateASTF b)
 
 --------------------------------------------------------------------------------
 -- Fin.
